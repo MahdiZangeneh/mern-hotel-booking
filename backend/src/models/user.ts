1e8f19902324341e5983fff37ad9241309ec8de2
Mz <mahdi.zangeneh@whipmobility.com>
2024-02-07 13:02:39 +0800

    added feature to add hotel, and e2e test for feature

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import { UserType } from "../shared/types";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema<UserType>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
